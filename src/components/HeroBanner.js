2c885804875ff21889d520fb1cf734fe94d5da0e
salmaQueen <salmawasee9@gmail.com>
2023-01-18 16:53:29 +0300

    updated

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import HeroBannerImage from '../assets/images/banner.png';
+// import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
-  <Box sx={{mb:{lg: '200px'} , mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' }, mb: { lg: '200px' } }} position="relative" p="20px">
     <Typography color="#2E8BC0" fontWeight="600" fontSize="26px">Fitness Club</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
       Sweat, Smile <br />
